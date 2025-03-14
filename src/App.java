6d5f8ed74d3fcd40eb099b2d4737275781fc9c3d
Juantoralx <jetorradoal@ufpso.edu.co>
2025-03-14 07:46:19 -0500

    Actualización en Números Mágcos

--- src/App.java
+++ src/App.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner (System.in);
+        System.out.println("Ingresa un número");
+        int num = input.nextInt();
+        System.out.println("El número es: " + num);
+        if (num <= 0) {
+            System.out.print("Número no valido");
+            input.close();
+            return;  
+        }
+        for (int K = 1 ; K<=num ; K++ ) {
+            for (int j=1 ; j<=K ; j++ ) {
+                if (j%2==1) {
+                    System.out.print(K);
+                } else {
+                    System.out.print(K*-1);
+                }
+                if (j < K  ) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        
     }
 }
